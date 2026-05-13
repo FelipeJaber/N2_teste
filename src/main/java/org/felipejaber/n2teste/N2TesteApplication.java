6681114d6d6fc75d272f356b7a4df13ea66bad69
Felipe <felipejaber.dev@gmail.com>
2026-05-13 14:53:32 -0300

    init

---
+++ src/main/java/org/felipejaber/n2teste/N2TesteApplication.java
@@ -0,0 +1,13 @@
+package org.felipejaber.n2teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N2TesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N2TesteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/felipejaber/n2teste/N2TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.felipejaber.n2teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N2TesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
